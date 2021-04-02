ce59b22b8709d23e1beb12cb0794ebf6d2851d59
Falk Rund <Un1matr1x@users.noreply.github.com>
2021-04-02 21:01:25 +0200

    [main] add MapController with symfony console by: $ php .\bin\console make:controller MapController

---
+++ src/Controller/MapController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MapController extends AbstractController
+{
+    #[Route('/map', name: 'map')]
+    public function index(): Response
+    {
+        return $this->render('map/index.html.twig', [
+            'controller_name' => 'MapController',
+        ]);
+    }
+}
