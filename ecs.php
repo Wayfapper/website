17d489e7cf62f898bbbef67d991353ccaab76fa5
Falk Rund <Un1matr1x@users.noreply.github.com>
2021-04-10 12:24:06 +0200

    [grumphp/ecs] fix StyleCi issues https://github.styleci.io/analyses/BMeJwr

--- ecs.php
+++ ecs.php
@@ -16,8 +16,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ]);
 
     $parameters->set(Option::SETS, [
