488b6af16a4443c295088e76287e348370cc1da7
Falk Rund <Un1matr1x@users.noreply.github.com>
2021-05-02 16:31:45 +0200

    [map/settings] add location storage the stored location is not used at this stage

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,9 @@
 var attrOsm = 'Map data &copy; <a href="https://openstreetmap.org/">OpenStreetMap</a> contributors';
 var attrOverpass = 'POI via <a href="https://www.overpass-api.de/">Overpass API</a>';
 
+/**
+ * initialisation of the map
+ */
 function initMap() {
     var osm = new L.tileLayer(mapStyle,{
         maxZoom: 19,
@@ -17,7 +20,80 @@ function initMap() {
 
         viewMyLocLayer = new L.LayerGroup();
         viewMyLocLayer.addTo(map);
+
+        map.on('moveend', function() {
+            updateLocation();
+        });
 }
+
+/**
+ * store the position in the localstorage and the url
+ */
+function updateLocation() {
+    let mapCenter = map.getBounds().getCenter();
+    let mapZoom = map.getZoom();
+    storeSetting('mapCenter', mapCenter);
+    storeSetting('mapZoom', mapZoom);
+    let url = location.href;
+    let urlLat = addParam(url, "lat", mapCenter.lat);
+    let urlLng = addParam(urlLat, "lng", mapCenter.lng);
+    let urlZ = addParam(urlLng, "z", mapZoom);
+    window.history.pushState({path:urlZ},'',urlZ);
+}
+
+/**
+ * store data in localstorage with a provided key
+ * @param {string} key      the name of the key
+ * @param {string} value    the data to store
+ */
+function storeSetting(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+/**
+ * load data from localstorage stored with a provided key
+ * @param {string} key  the name of the key
+ * @return {String}     the stored value, null if data doesn't exist
+ */
+function retrieveSetting(key) {
+    var value;
+    if (localStorage.getItem(key) !== 'undefined') {
+        value = JSON.parse(localStorage.getItem(key));
+    } else {
+        value = null;
+    }
+    return value;
+}
+
+/**
+ * Get the value of a querystring
+ * @param  {String} field the field to get the value of
+ * @param  {String} url   the URL to get the value from (optional)
+ * @return {String}       the field value
+ */
+ var getQueryString = function ( field, url ) {
+	var href = url ? url : window.location.href;
+	var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+	var string = reg.exec(href);
+	return string ? string[1] : null;
+};
+
+/**
+ * Modifiy a given url
+ * @param  {String} url   the URL to get the value from
+ * @param  {String} param the field to get the value of
+ * @param  {String} value the field to get the value of
+ * @return {String}       the new url
+ */
+function addParam(url, param, value) {
+    var a = document.createElement('a'), regex = /(?:\?|&amp;|&)+([^=]+)(?:=([^&]*))*/g;
+    var match, str = []; a.href = url; param = encodeURIComponent(param);
+    while (match = regex.exec(a.search))
+        if (param != match[1]) str.push(match[1]+(match[2]?"="+match[2]:""));
+    str.push(param+(value?"="+ encodeURIComponent(value):""));
+    a.search = str.join("&");
+    return a.href;
+ }
 
 var locationButton = new L.easyButton({
     states:[{
@@ -27,7 +103,7 @@ var locationButton = new L.easyButton({
         onClick: function(control){
             control.state("loading");
             control._map.on('locationfound', function(e){
-                viewMyLocLayer.clearLayers()
+                viewMyLocLayer.clearLayers();
                 this.setView(e.latlng, 17);
                 var radius = e.accuracy / 2;
                 var marker = new L.Marker.SVGMarker(e.latlng).bindPopup("You are within " + Math.round(radius) + " meters from this point").openPopup();
@@ -39,7 +115,7 @@ var locationButton = new L.easyButton({
             control._map.on('locationerror', function(){
                 control.state('error');
             });
-            control._map.locate()
+            control._map.locate();
         },
     }, {
         stateName: 'loading',
