5ab5e1f9fbbb6f20d6a519bb8e9b1c47ba1cfffb
Falk Rund <Un1matr1x@users.noreply.github.com>
2021-05-21 12:53:32 +0000

    Apply fixes from StyleCI

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -11,9 +11,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @method App\Entity\User|null find($id, $lockMode = null, $lockVersion = null)
- * @method User|null findOneBy(array $criteria, array $orderBy = null)
- * @method User[]    findAll()
- * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method User|null            findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]               findAll()
+ * @method User[]               findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  * @extends \Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository<UserRepository>
  * @psalm-suppress all
  */
