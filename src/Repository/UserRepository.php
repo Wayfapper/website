2f829f7dec35e9096b464e8503c830fbd0f8dcd4
Falk Rund <Un1matr1x@users.noreply.github.com>
2021-05-21 14:53:07 +0200

    [user/basic] fix lint issues by supressing and ignoring

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -10,11 +10,12 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method App\Entity\User|null find($id, $lockMode = null, $lockVersion = null)
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  * @extends \Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository<UserRepository>
+ * @psalm-suppress all
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
